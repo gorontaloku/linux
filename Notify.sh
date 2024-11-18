52f036f6c44db3fa0ec15f5739910eddb580b6c5
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-11-18 19:04:56 +0800

    Add files via upload

---
+++ Notify.sh
@@ -0,0 +1,13 @@
+sleep 10
+
+notify-send "Haii..
+Selamat Datang Kembali 🥰"  -t 5000
+
+sleep 6
+
+notify-send "⚠️ Pastikan Settingan Linux ini beli dari :" "        BM
+        082292471256" -t 4000
+
+sleep 4
+
+notify-send "Dilarang Keras" "Menyebarkan/menjual lagi file settingan Linux ini ke orang lain tanpa seizin dia." -t 4000
