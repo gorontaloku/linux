01c0fb5fd0706a8bf172b29f5528dcc95e7b51b5
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-04 15:15:31 +0800

    Create termux.sh

---
+++ style/termux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pkg install update
+pkg install upgrade -y
+pkg install git
+pkg install wget
+pkg install zsh
+pkg install lsd
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+termux-reloud-settings
+
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+nano .zshrc
+
+
+git clone https://github.com/adi1090x/termux-style
+
+cd termux-style
+
+./install
