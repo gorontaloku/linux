f1edf7b29383163bcdf75dde45aa2d4f42b0e6e6
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-05 02:29:58 +0700

    Update termux.sh

--- style/termux.sh
+++ style/termux.sh
@@ -8,9 +8,6 @@ pkg install python
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-exit
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 nano .zshrc
