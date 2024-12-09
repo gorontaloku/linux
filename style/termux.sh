b3c9dda33bbc99c3b886d9005405c1472ea134c6
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-10 03:07:55 +0800

    Update termux.sh

--- style/termux.sh
+++ style/termux.sh
@@ -9,7 +9,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-nano .zshrc
+echo "ZSH_THEME="powerlevel10k/powerlevel10k" >> #HOME/.zshrc
+echo "alias ls="lsd" >> #HOME/.zshrc
 
 git clone https://github.com/adi1090x/termux-style
 
@@ -24,5 +25,7 @@ cd
 wget https://raw.githubusercontent.com/gorontaloku/linux/refs/heads/main/tampilan.py
 mv tampilan.py /data/data/com.termux/files/usr/etc/
 
-cd /.oh-my-zsh
-nano zshrc
+echo "clear" >> $PREFIX/etc/zshrc
+echo "cd ../usr/etc" >> $PREFIX/etc/zshrc
+echo "python tampilan.py" >> $PREFIX/etc/zshrc
+echo "cd" >> $PREFIX/etc/zshrc
