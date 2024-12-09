07928bf4a579640f2c1a349d657ebcc8d752a095
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-10 04:06:31 +0800

    Update termux.sh

--- style/termux.sh
+++ style/termux.sh
@@ -1,18 +1,13 @@
-
-pkg install git
-pkg install wget
-pkg install zsh
-pkg install lsd
-pkg install python
+pkg install git -y
+pkg install wget -y
+pkg install zsh -y
+pkg install lsd -y
+pkg install python 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo "ZSH_THEME='powerlevel10k/powerlevel10k'" >> /data/data/com.termux/files/home/.zshrc
-echo "
-alias ls='lsd '
-" >> data/data/com.termux/files/home/.zshrc
 
 git clone https://github.com/adi1090x/termux-style
 
@@ -26,6 +21,10 @@ cd
 
 wget https://raw.githubusercontent.com/gorontaloku/linux/refs/heads/main/tampilan.py
 mv tampilan.py /data/data/com.termux/files/usr/etc/
+
+echo "ZSH_THEME='powerlevel10k/powerlevel10k'" >> /data/data/com.termux/files/home/.zshrc
+echo "
+alias ls='lsd'" >> data/data/com.termux/files/home/.zshrc
 
 echo "clear" >> $PREFIX/etc/zshrc
 echo "cd ../usr/etc" >> $PREFIX/etc/zshrc
