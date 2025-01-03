d02634f100d396c2f5652430870825121d18311e
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2025-01-03 15:35:52 +0800

    Update utils.sh

--- ubuntu/utils.sh
+++ ubuntu/utils.sh
@@ -4,34 +4,24 @@
 cat <<'EOF' > start
 #!/bin/bash
 
-# Start PulseAudio
-pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 > /dev/null 2>&1
-
-XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1.0 & > /dev/null 2>&1
-sleep 1
-
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
-sleep 1
-
-virgl_test_server_android --angle-gl & > /dev/null 2>&1
-
-proot-distro login debian --user user --shared-tmp -- bash -c "export PULSE_SERVER=127.0.0.1 env DISPLAY=:1.0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session startxfce4 & > /dev/null 2>&1 "
-
-sleep 5
-process_id=$(ps -aux | grep '[x]fce4-screensaver' | awk '{print $2}')
-kill "$process_id" > /dev/null 2>&1
-
-#pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
-
 # Kill all old processes
-#killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
+killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
 
+# Start Termux X11
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
+XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 -ac &
+sleep 3
 
-#sleep 3
+# Start PulseAudio
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 
+# Start GPU accelerated virglserver
+virgl_test_server_android &
 
 # Log in to proot debian and start the desktop environment. Note that startxfce4 at the end is used to start the XFCE4 desktop.
-#proot-distro login debian --user user --shared-tmp -- bash -c "export PULSE_SERVER=127.0.0.1 > /dev/null 2>&1 "
+proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
 
 EOF
 
