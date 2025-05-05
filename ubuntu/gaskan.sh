c1f5530b3b644aef6b194b8bd8ad4573a9248489
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2025-05-06 00:16:24 +0800

    Update gaskan.sh

--- ubuntu/gaskan.sh
+++ ubuntu/gaskan.sh
@@ -97,9 +97,9 @@ chmod +x utils.sh
 
 
 #Download script banner
-wget https://raw.githubusercontent.com/gorontaloku/linux/main/style/termux.sh
-chmod +x termux.sh
-./termux.sh
+#wget https://raw.githubusercontent.com/gorontaloku/linux/main/style/termux.sh
+#chmod +x termux.sh
+#./termux.sh
 
 #pd login debian
 #cd /tmp
