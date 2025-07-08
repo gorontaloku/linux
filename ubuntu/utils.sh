56c6976d71ec4d76360c186e56f7dde5e3808a98
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2025-07-08 12:19:13 +0800

    Update utils.sh

--- ubuntu/utils.sh
+++ ubuntu/utils.sh
@@ -10,7 +10,7 @@ killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-
 # Start Termux X11
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 XDG_RUNTIME_DIR=${TMPDIR}
-termux-x11 :0 -ac &
+termux-x11 :1 -ac &
 sleep 3
 
 # Start PulseAudio
@@ -21,7 +21,7 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymou
 virgl_test_server_android &
 
 # Log in to proot debian and start the desktop environment. Note that startxfce4 at the end is used to start the XFCE4 desktop.
-proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
+proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:1 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
 
 EOF
 
